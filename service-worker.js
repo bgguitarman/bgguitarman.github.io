2ae24e9f41cf73e5045e216ae9c54ef1eb0c31a8
bgguitarman <bgguitarman@gmail.com>
2016-10-25 09:29:51 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,39 @@
 'use strict';
 
 self.addEventListener('push', function(event) {
-  console.log('Received a push message', event);
 
-  var title = 'Yay a message.';
-  var body = 'We have received a push message.';
-  var icon = '/images/icon-192x192.png';
-  var tag = 'simple-push-demo-notification-tag';
+var apiPath = '<apiPath>';
+event.waitUntil(registration.pushManager.getSubscription().then(function (subscription){
 
-  event.waitUntil(
-    self.registration.showNotification(title, {
-      body: body,
-      icon: icon,
-      tag: tag
+    return fetch(apiPath).then(function(response){
+        if(response.status !== 200){
+            throw new Error();
+        }
+
+        return response.json().then(function(data){
+            var title = data.title;
+            var message = data.body;
+            var icon = data.icon;
+            var tag = data.tag;
+            var url = data.url;
+            return self.registration.showNotification(title,{
+               body: message,
+               icon: icon,
+               tag: tag,
+               data: url
+            });
+        })
+    }).catch(function(err){
+
     })
-  );
+
+}));
+return;
 });
 
 self.addEventListener('notificationclick', function(event) {
   console.log('On notification click: ', event.notification.tag);
-  // Android doesn’t close the notification when you click on it
+  // Android doesnâ€™t close the notification when you click on it
   // See: http://crbug.com/463146
   event.notification.close();
 
